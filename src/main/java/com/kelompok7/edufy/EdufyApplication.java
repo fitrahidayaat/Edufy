7cda159e7746e30ca4377de96db7b98c8a11dc53
Fitra Hidayat <99539325+fitrahidyt@users.noreply.github.com>
2023-11-11 14:13:58 +0700

    creating template springboot project

---
+++ src/main/java/com/kelompok7/edufy/EdufyApplication.java
@@ -0,0 +1,13 @@
+package com.kelompok7.edufy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdufyApplication.class, args);
+	}
+
+}
