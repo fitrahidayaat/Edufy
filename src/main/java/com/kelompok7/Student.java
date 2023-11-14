d62e6fdae02f1e6f53f672bc0c1f1e1e0938a078
HandyPuspaNugraha <handypuspanugraha@gmail.com>
2023-11-14 17:57:36 +0700

    Student class 0.01

---
+++ src/main/java/com/kelompok7/Student.java
@@ -0,0 +1,14 @@
+package com.kelompok7;
+
+public class Student extends User{
+    private int gradeLevel;
+    private String homeRoomTeacher;
+
+    public void viewAssigment(String courseID){
+    
+    }
+
+    public void submitAssigment(String assigmentID, file submision){
+        
+    }
+}
