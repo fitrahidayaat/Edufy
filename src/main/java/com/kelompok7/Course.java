aea8bed9e0ab7772791a5e737fe8dfa3e55b4323
Handy Puspa Nugraha <handypuspanugraha@gmail.com>
2023-11-16 21:26:21 +0700

    Assigment class dan Course class 0.01

--- src/main/java/com/kelompok7/Course.java
+++ src/main/java/com/kelompok7/Course.java
@@ -3,17 +3,21 @@ package com.kelompok7;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Assigment {
-    private String assignmentID;
-    private Course course;
-    private Date date;
-    private List<Submision> submisionList = new ArrayList<Submision>();
+public class Course {
+    private String courseID;
+    private String courseName;
+    private Teacher teacher;
+    private List<Student> studentList=new ArrayList<Student>();
+    private List<Assigment> assignments=new ArrayList<Assigment>();
+    private List<Material> materials=new ArrayList<Material>();
+
+    public void addStudent(String studentID){
+
+    }
+
+    public void removeStudent(String studentID){
+
+    }
     
-    public void submit(String studentID, File submision){
 
-    }
-
-    public void grade(String studentID, int grade){
-
-    }
 }
