6fb09b6bdaf39db149a4adb0f0d7306278e76ce3
Handy Puspa Nugraha <handypuspanugraha@gmail.com>
2023-11-16 21:21:15 +0700

    Tacher class 0.01

---
+++ src/main/java/com/kelompok7/Admin.java
@@ -0,0 +1,19 @@
+package com.kelompok7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Assigment {
+    private String assignmentID;
+    private Course course;
+    private Date date;
+    private List<Submision> submisionList = new ArrayList<Submision>();
+    
+    public void submit(String studentID, File submision){
+
+    }
+
+    public void grade(String studentID, int grade){
+
+    }
+}
