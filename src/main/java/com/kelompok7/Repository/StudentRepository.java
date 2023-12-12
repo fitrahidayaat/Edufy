1501a9e0e8308716bee7e291b5e93063196bc271
Handy Puspa Nugraha <handypuspanugraha@gmail.com>
2023-12-12 17:57:51 +0700

    Fixing detached head

---
+++ src/main/java/com/kelompok7/Repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.kelompok7.Repository;
+
+import com.kelompok7.Model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+}
