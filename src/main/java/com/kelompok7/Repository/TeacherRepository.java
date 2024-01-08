1c33348f30e4b2b82a13cbf0fdcccbdda1301f7d
fidelaazifah <fidelaazifah26@gmail.com>
2024-01-08 21:34:41 +0700

    remove to merge

--- src/main/java/com/kelompok7/Repository/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kelompok7.Repository;
-
-import com.kelompok7.Model.Student;
-import com.kelompok7.Model.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-    Teacher findByEmailAndPassword(String email, String password);
-}
